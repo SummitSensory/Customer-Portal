b9bbaee64040a9587704b6fbf0079c1afdec3ac0
SummitSensory <sales@summitsensory.com>
2026-07-03 10:12:56 -0600

    Delete pages directory

--- pages/api/auth/signout-customer.js
+++
@@ -1,7 +0,0 @@
-import { serialize } from 'cookie';
-import { SESSION_COOKIE, clearCookieOptions } from '../../../lib/auth';
-
-export default function handler(req, res) {
-  res.setHeader('Set-Cookie', serialize(SESSION_COOKIE, '', clearCookieOptions()));
-  res.redirect(307, '/');
-}
